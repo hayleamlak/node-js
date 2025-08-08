c476383c2f221db38dd35ba0479d7b5b9e7c4c44
hayleamlak <hayleamlaktesfaye12@gmail.com>
2025-08-08 11:13:06 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    // Read the HTML file and send it
+    const filePath = path.join(__dirname, 'index.html');
+
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error loading page');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Page Not Found');
+  }
+});
+
+server.listen(5000, () => {
+  console.log('Server running at http://localhost:5000');
+});
